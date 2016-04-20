853487a21d076f538980c16fcc04df3e338afb47
Ram Bavireddi <ramu.bavireddi@gmail.com>
2016-04-20 18:50:22 +0530

    Add Scatter Gather With CustomAggregationStrategy

---
+++ scatter-gather/src/main/java/org/ram/mule/examples/MergingAggregationStrategy.java
@@ -0,0 +1,40 @@
+package org.ram.mule.examples;
+
+import java.util.List;
+
+import org.mule.DefaultMessageCollection;
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.MuleMessageCollection;
+import org.mule.api.routing.AggregationContext;
+import org.mule.routing.AggregationStrategy;
+
+public class MergingAggregationStrategy implements AggregationStrategy {
+
+	@Override
+	public MuleEvent aggregate(AggregationContext context) throws MuleException {
+		MuleEvent resultEvent = null;
+		MuleEvent previous = context.getOriginalEvent();
+		MuleContext muleContext = previous.getMuleContext();
+		MuleMessageCollection coll = new DefaultMessageCollection(muleContext);
+
+		for (MuleEvent event : context.collectEventsWithoutExceptions()) {
+			List<?> payload = (List<?>) event.getMessage().getPayload();
+			for (Object element : payload) {
+				MuleMessage message = new DefaultMuleMessage(element, muleContext);
+				coll.addMessage(message);
+			}
+		}
+
+		resultEvent = new DefaultMuleEvent(coll, previous, previous.getSession());
+
+		for (String name : previous.getFlowVariableNames()) {
+			resultEvent.setFlowVariable(name, previous.getFlowVariable(name));
+		}
+		return resultEvent;
+	}
+}
